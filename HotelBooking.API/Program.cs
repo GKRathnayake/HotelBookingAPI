a38089cca7a9893d16eff1fe97469d0f430ca9d8
Gayan Krishantha Rathnayake <krishanthaenet@gmail.com>
2022-11-10 07:25:30 +0530

    Registering database context.

--- HotelBooking.API/Program.cs
+++ HotelBooking.API/Program.cs
@@ -1,3 +1,7 @@
+using HotelBooking.Infrastructure;
+using HotelBooking.Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+var connectionString = builder.Configuration.GetConnectionString("Hotel");
+builder.Services.AddDbContext<AppDbContext>(x => x.UseSqlServer(connectionString));
+
+builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
 
